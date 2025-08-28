574b742efe502abf752d2b1f43e3f85b22b77770
Joseph Rissman <jirissman@gmail.com>
2025-08-28 16:00:52 -0400

    refactor(HomePage, About): replace image handling with ImageWrapper component
    - Updated HomePage to utilize the new ImageWrapper component for image rendering, improving consistency across the application.
    - Modified About component to also use ImageWrapper, enhancing image management and reducing direct dependencies on Sanity's image handling.
    - Adjusted CATEGORY_QUERY to include a more structured photo object, allowing for better image data handling.

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -3,7 +3,14 @@ import { defineQuery } from "next-sanity";
 export const CATEGORY_QUERY = defineQuery(`*[
   _type == "category"
   && defined(slug.current)
-]|order(title asc)[0...3]{_id, title, slug, description, coverPhoto}`);
+]|order(title asc)[0...3]{
+  _id, title, slug, description, "photo": coverPhoto{
+    "id": asset->_id,
+   "url": asset->url,
+   alt,
+   "preview": asset->metadata.lqip,
+  }
+  }`);
 
 export const PHOTO_QUERY = defineQuery(`*[
 _type == "category" &&
